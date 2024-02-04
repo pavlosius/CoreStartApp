f57ab7bf7cceaa070c73750f52094a9dfc6d82c9
pavlosius <pavlosius@mail.ru>
2024-02-04 20:08:37 +0300

    Задание 32.5.5

--- CoreStartApp/Program.cs
+++ CoreStartApp/Program.cs
@@ -14,32 +14,6 @@ if (env.IsDevelopment() || env.IsStaging())
 
 app.UseRouting();
 
-//app.Use(async (context, next) =>
-//{
-//    // Строка для публикации в лог
-//    string logMessage = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
-
-//    // Путь до лога (опять-таки, используем свойства IWebHostEnvironment)
-//    string logFilePath = Path.Combine(env.ContentRootPath, "Logs", "RequestLog.txt");
-
-//    // Используем асинхронную запись в файл
-//    await File.AppendAllTextAsync(logFilePath, logMessage);
-
-//    Console.WriteLine(logMessage);
-
-//    await next.Invoke();
-//});
-
-////Добавляем компонент для логирования запросов с использованием метода Use.
-//app.Use(async (context, next) =>
-//{
-//    // Для логирования данных о запросе используем свойства объекта HttpContext
-//    Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
-//    await next.Invoke();
-//});
-
-//app.MapGet("/", () => "Hello World!");
-
 app.UseMiddleware<LoggingMiddleware>();
 
 app.UseEndpoints(endpoints =>
@@ -49,7 +23,6 @@ app.UseEndpoints(endpoints =>
         await context.Response.WriteAsync($"Welcome to the {env.ApplicationName}!");
     });
 });
-
 
 app.Map("/about", About);
 
@@ -61,11 +34,6 @@ app.Run(async (context) =>
 });
 
 app.Run();
-
-//app.Run(async (context) =>
-//{
-//    await context.Response.WriteAsync($"Welcome to the {env.ApplicationName}!");
-//});
 
 
 static void About(IApplicationBuilder app)
@@ -80,7 +48,6 @@ static void About(IApplicationBuilder app)
 
 static void Config(IApplicationBuilder app)
 {
-    // Получаем сервис IWebHostEnvironment из IApplicationBuilder
     var env = app.ApplicationServices.GetService<IWebHostEnvironment>();
 
     app.Run(async context =>
